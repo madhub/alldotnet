aa49717fc900b04ed6f5dc831ed4eb1e400410d0
madhub <b.madhu@outlook.com>
2018-06-05 22:37:48 +0530

    .

---
+++ ConsoleHostBuilderDemo/DemoService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleHostBuilderDemo
+{
+    public class DemoService : IHostedService, IDisposable
+    {
+        
+        private Timer _timer;
+        private readonly ILogger<DemoService> _logger;
+        public DemoService(ILogger<DemoService> logger)
+        {
+            this._logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting");
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            _logger.LogInformation($"Background work with text: ....");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+        
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~DemoService() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
